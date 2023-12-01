38202e64420a6869070fdfc30fd1473d70fc7385
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2023-11-30 23:24:20 -0800

    changes

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -6,8 +6,8 @@ export default function Skills() {
       <div className="flex-shrink-0 w-80 mr-40 text-secondary uppercase md:text-right font-bold tracking-wide">
         SKILLS 👨‍💻
       </div>
-      <div className="flex flex-wrap shrink-0 justify-between w-5/12">
-        <div className="mb-6">
+      <div className="flex flex-wrap justify-between">
+        <div className="mb-6 flex-1 min-w-[150px]">
           <div className="font-bold mb-4">LANGUAGES</div>
           <ul>
             <li className="mb-2 text-base font-normal leading-6">Javascript(ES6)</li>
@@ -18,7 +18,7 @@ export default function Skills() {
             <li className="mb-2 text-base font-normal leading-6">SQL</li>
           </ul>
         </div>
-        <div className="mb-6">
+        <div className="mb-6 flex-1 min-w-[150px]">
           <div className="font-bold mb-4">TOOLS</div>
           <ul>
             <li className="mb-2 text-base font-normal leading-6">Git & Github</li>
@@ -28,7 +28,7 @@ export default function Skills() {
             <li className="mb-2 text-base font-normal leading-6">Chrome DevTools</li>
           </ul>
         </div>
-        <div className="mb-6">
+        <div className="mb-6 flex-1 min-w-[150px]" >
           <div className="font-bold mb-4">FRAMEWORKS/LIBRARIES</div>
           <ul>
             <li className="mb-2 text-base font-normal leading-6">React</li>
