764117fec835d3b628fe9c77ceb7d4d38652d472
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2023-11-30 20:56:00 -0800

    changes

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="flex flex-col md:flex-row gap-1 mt-8">
+    <section className="flex flex-col md:flex-row gap-1 p-7 mt-8">
        <div className="flex-shrink-0 w-80 mr-40 text-secondary uppercase md:text-right font-bold tracking-wide">
         BACKGROUND 👨‍💻
       </div>
-      <div className="pt-10 pl-10 md:p-0 font-light w-full max-w-3xl text-justify">
+      <div className= "font-light w-full max-w-3xl text-justify">
         <p className="mb-10 ">
           Welcome to my portfolio! I hold a degree in Information Science and Engineering from 
           {' '}
