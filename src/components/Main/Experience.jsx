19ae01e1104b1dc542778207f346fe4d844cfbad
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2023-12-02 21:07:15 +0530

    changes

--- src/components/Main/Experience.jsx
+++ src/components/Main/Experience.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Experience({ company, position, location, duration, description, techStack }) {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col md:justify-center md:items-center">
       <div className="flex flex-col w-9/12 mt-5 border-b border-[#232323] ">
         <div className="flex justify-between">
           <div>
